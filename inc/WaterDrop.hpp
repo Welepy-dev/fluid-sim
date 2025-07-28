6ebde8d531e8cda7256280bf4fca5eb11a29d57b
Welepy-dev <mwsilva.contato@outlook.com>
2025-07-29 00:33:12 +0100

    more logic for the drop

--- inc/WaterDrop.hpp
+++ inc/WaterDrop.hpp
@@ -2,7 +2,19 @@
 
 # include "raylib.h"
 
-#define WaterDropRadius 3
+#define WaterDropRadius 5
+#define GRAVITY (Vector2) {0, 98} 
+#define Vector2DOWN (Vector2) {0, 1}
+#define Vector2UP (Vector2) {0, -1}
+#define Vector2LEFT (Vector2) {-1, 0}
+#define Vector2RIGHT (Vector2) {1, 0}
+#define ORIGIN (Vector2) {0, 0}			//Screen Origin
+#define Vector2Zero (Vector2) {0, 0}
+#define Vector2One (Vector2) {1, 1}
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 1200
+#define DAMPING 0.49f
+#define MAX_DROPLETS 1000
 
 class WaterDrop
 {
@@ -13,6 +25,7 @@ class WaterDrop
 		~WaterDrop();
 
 		void	render(void);
+		void	checkCollision(void);
 
 	private:
 		Vector2		_pos;
--- src/classes/WaterDrop.cpp
+++ src/classes/WaterDrop.cpp
@@ -2,6 +2,54 @@
 
 WaterDrop::WaterDrop(Vector2 pos, Vector2 vel): _radius(WaterDropRadius) { this->_pos = pos; this->_vel = vel; }
 
+WaterDrop::WaterDrop(const WaterDrop &other): _radius(other._radius) { this->_pos = other._pos; this->_vel = other._vel; }
+
+WaterDrop &WaterDrop::operator=(const WaterDrop &other)
+{ 
+	if (this != &other)
+	{
+		this->_pos = other._pos; 
+		this->_vel = other._vel; 
+	}
+	return *this;
+}
+
 WaterDrop::~WaterDrop( ) { }
 
-void	WaterDrop::render(void) { DrawCircleV(_pos, _radius, DARKBLUE); }
+void	WaterDrop::checkCollision(void)
+{
+	if (_pos.y + _radius >= WINDOW_HEIGHT)
+	{
+		_pos.y = WINDOW_HEIGHT - _radius;
+		_vel.y *= -1 * DAMPING;
+	}
+	else if (_pos.y <= ORIGIN.y)
+	{
+		_pos.y = ORIGIN.y;
+		_vel.y = 1 * DAMPING;
+	}
+
+	if (_pos.x + _radius >= WINDOW_WIDTH)
+	{
+		_pos.x = WINDOW_WIDTH - _radius;
+		_vel.x *= -1 * DAMPING;
+	}
+	else if (_pos.x <= ORIGIN.x)
+	{
+		_pos.x = ORIGIN.x + _radius;
+		_vel.x *= -1 * DAMPING;
+	}
+}
+
+void	WaterDrop::render(void)
+{
+	// Apply gravity to velocity (acceleration)
+	_vel += GRAVITY * GetFrameTime();
+	
+	// Update position based on velocity
+	_pos += _vel * GetFrameTime();
+
+	DrawCircleV(_pos, _radius, DARKBLUE);
+	checkCollision();
+
+}
